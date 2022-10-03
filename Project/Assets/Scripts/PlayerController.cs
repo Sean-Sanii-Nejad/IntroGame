467d31154587d2b3fa126468e98f8eccdd8eb051
Sean-Sanii-Nejad <sean-saniinejad@hotmail.co.uk>
2022-10-03 23:25:37 +0100

    Lab 2 stuff #4

--- Project/Assets/Scripts/PlayerController.cs
+++ Project/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,12 @@ public class PlayerController : MonoBehaviour {
 
      public Vector2 moveValue ;
      public float speed ;
+     private int count;
+
+     void Start()
+     {
+         count = 0;
+     }
      void OnMove ( InputValue value ) 
      {
         moveValue = value . Get < Vector2 >() ;
@@ -16,5 +22,13 @@ public class PlayerController : MonoBehaviour {
      {
          Vector3 movement = new Vector3 ( moveValue .x , 0.0f , moveValue . y );
          GetComponent<Rigidbody>().AddForce( movement * speed * Time.fixedDeltaTime );
+     }
+     
+     void OnTriggerEnter ( Collider other ) {
+          if (other.gameObject.tag == "PickUp")
+          {
+              count++;
+              other.gameObject.SetActive(false);
+          }
      }
  }
